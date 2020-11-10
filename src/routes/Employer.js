dcfa3648bd01753230e54dd069a9c7de7a8c8457
techid2000 <techid2000@gmail.com>
2020-11-10 18:01:02 +0700

    ✨ add Employer route

---
+++ src/routes/Employer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { EmployerJob, EmployerJobCreate } from '.';
+
+const Employer = () => {
+  const { path } = useRouteMatch();
+  return (
+    <div>
+      Employer
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <div>Employer Main</div>
+        </Route>
+        <Route path={`${path}/job/:jobId`}>
+          <EmployerJob />
+        </Route>
+        <Route path={`${path}/job/create`}>
+          <EmployerJobCreate />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default Employer;
