4b76882ea4788ef21f3f9187b9d1391201b0a5d2
techid2000 <techid2000@gmail.com>
2020-11-10 23:04:21 +0700

    ✨ add EmployerJobItem

---
+++ src/components/EmployerJobItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { List, Tag, Space, Divider, Typography, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Link, useParams } from 'react-router-dom';
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
+
+const EmployerJobItem = ({
+  job: {
+    id,
+    title,
+    description,
+    fieldOfWork,
+    minCompensation,
+    maxCompensation,
+    minQuota,
+    maxQuota,
+    isMain,
+  },
+}) => {
+  const { email } = useParams();
+
+  return (
+    <List.Item key={id}>
+      <List.Item.Meta
+        title={
+          <Link to={`/employer/${email}/job/${id}`}>
+            <Typography.Link type="link">{title}</Typography.Link>
+          </Link>
+        }
+        description={fieldOfWork.map((field) => (
+          <Tag>{field}</Tag>
+        ))}
+      />
+      <Space direction="vertical">
+        <div>{description}</div>
+        <div>
+          <Typography.Text strong>
+            {`${minCompensation / 1000}k - ${maxCompensation / 1000}k`}
+          </Typography.Text>
+          <Divider type="vertical" />
+          <IconText icon={UserOutlined} text={`${minQuota} - ${maxQuota}`} />
+        </div>
+      </Space>
+    </List.Item>
+  );
+};
+
+export default EmployerJobItem;
