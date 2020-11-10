38dc811a6897f80ef08fa804e2a90f701e543e66
Wachirapat M <wachi_tony@yahoo.co.th>
2020-11-11 00:01:40 +0700

    fix server/index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const app = require('./app');
+const express = require('express')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+require('./controllers/temp.controllers')(app)
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`)
 });
