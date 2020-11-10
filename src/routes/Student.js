5e239c9814c05d20d0da205bcc02edf7c5e0484e
techid2000 <techid2000@gmail.com>
2020-11-10 18:01:47 +0700

    ✨ add Student route

---
+++ src/routes/Student.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { StudentApplication, StudentApplyJob } from '.';
+
+const Student = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <div>
+      Student
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <div>Student Main</div>
+        </Route>
+        <Route path={`${path}/application/:applicationId`}>
+          <StudentApplication />
+        </Route>
+        <Route path={`${path}/apply/:jobId`}>
+          <StudentApplyJob />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default Student;
