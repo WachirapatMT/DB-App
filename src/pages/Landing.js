5d09fa3774db3a311b96714445251fa2a6efbc49
unknown <chain011@yahoo.co.th>
2020-11-11 16:05:32 +0700

    landing to student

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button, Radio } from 'antd';
 
 const Landing = () => {
-  return <div>Landing</div>;
+  const onFinish = () => {
+    window.location.href = userType + '/' + email;
+    console.log('Success:', userType, email, password);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  const [userType, setUserType] = useState('student');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onUserTypeChange = (e) => {
+    setUserType(e.target.value);
+  };
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Nisiter</h1>
+      </div>
+      <div>
+        <Radio.Group onChange={onUserTypeChange}>
+          <Radio.Button value="student">Student</Radio.Button>
+          <Radio.Button value="employer">Employer</Radio.Button>
+        </Radio.Group>
+      </div>
+      <div>
+        <Input type="email" value={email} onChange={onEmailChange} />
+        <Input value={password} onChange={onPasswordChange} />
+      </div>
+      <Button
+        type="primary"
+        onClick={onFinish}
+        onError={(e) => onFinishFailed(e)}
+      >
+        Submit
+      </Button>
+    </div>
+  );
 };
 
 export default Landing;
