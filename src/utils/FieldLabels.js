010815b2ef251e895826fd728f764ef2092ff737
Rock <nattanon.hu@gmail.com>
2020-11-11 21:49:50 +0700

    ✨ add fieldLabels constant

---
+++ src/utils/FieldLabels.js
@@ -0,0 +1,12 @@
+const FIELD_LABELS = {
+  title: 'Title',
+  description: 'Description',
+  minCompensation: 'Minimum Compensation',
+  maxCompensation: 'Maximum Compensation',
+  minQuota: 'Minimum Quota',
+  maxQuota: 'Maximum Quota',
+  paymentMethod: 'Payment Method',
+  fieldsOfWork: 'Fields of work',
+};
+
+export default FIELD_LABELS;
