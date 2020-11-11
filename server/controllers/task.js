8b1a7d73434f7a5f6245e183432f3c96182855af
Wongtawan Junthai <wongtawan.wrk@gmail.com>
2020-11-11 21:14:24 +0700

    fixed task fields of work query

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -1,69 +1,95 @@
 const MySQL = require('../datasources/mysql.js');
 
 exports.Get = async (req, res) => {
-    const rows = await MySQL.Query(`select * from Task`);
-    rows.forEach((r, i) => {
-        rows[i] = {...r, gateway_data: JSON.parse(r.gateway_data)}
+    const rows = await MySQL.Query(`select * from Task natural join TaskFieldsOfWork`);
+    let fieldsOfWork = {}
+    let task = {}
+    rows.forEach(r => {
+        if (fieldsOfWork[r['task_id']] === undefined) fieldsOfWork[r['task_id']] = []
+        fieldsOfWork[r['task_id']].push(r['field_of_work']);
+        task[r['task_id']] = {
+            taskId: r['task_id'],
+            title: r['title'],
+            description: r['description'],
+            minCompensation: r['min_compensation'],
+            maxCompensation: r['max_compensation'],
+            minQuota: r['min_quota'],
+            maxQuota: r['max_quota'],
+            currentAccepted: r['current_accepted'],
+            paymentMethod: r['payment_method'],
+            isApproved: r['is_approved'],
+            paidAt: r['paid_at'],
+            gatewayData: JSON.parse(r['gateway_data']),
+            amount: r['amount'],
+            employerEmail: r['employer_email'],
+        };
     });
-    res.json(rows);
+
+    res.json(Object.values(task).map(t => ({...t, fieldsOfWork: fieldsOfWork[t['taskId']]})));
 }
 
 exports.Create = async (req, res) => {
     const {
         title,
         description,
-        min_compensation,
-        max_compensation,
-        min_quota,
-        max_quota,
-        current_accepted,
-        payment_method,
-        is_approved,
-        paid_at,
-        gateway_data,
+        minCompensation,
+        maxCompensation,
+        minQuota,
+        maxQuota,
+        currentAccepted,
+        paymentMethod,
+        isApproved,
+        paidAt,
+        gatewayData,
         amount,
-        employer_email,
+        employerEmail,
+        fieldsOfWork,
     } = req.body;
 
     const rows = await MySQL.Query(`insert into Task value (
         ${null},
         '${title}',
         '${description}',
-        ${min_compensation},
-        ${max_compensation},
-        ${min_quota},
-        ${max_quota},
-        ${current_accepted},
-        '${payment_method}',
-        ${is_approved},
-        '${paid_at}',
-        '${JSON.stringify(gateway_data)}',
+        ${minCompensation},
+        ${maxCompensation},
+        ${minQuota},
+        ${maxQuota},
+        ${currentAccepted},
+        '${paymentMethod}',
+        ${isApproved},
+        '${paidAt}',
+        '${JSON.stringify(gatewayData)}',
         '${amount}',
-        '${employer_email}'
+        '${employerEmail}'
     )`);
+
+    fieldsOfWork.forEach(f => {
+        MySQL.Query(`insert into TaskFieldsOfWork value (${rows.insertId}, '${f}')`).catch(e => console.log(e))
+    });
 
     res.json({
         id: rows.insertId,
         title,
         description,
-        min_compensation,
-        max_compensation,
-        min_quota,
-        max_quota,
-        current_accepted,
-        payment_method,
-        is_approved,
-        paid_at,
-        gateway_data,
+        minCompensation,
+        maxCompensation,
+        minQuota,
+        maxQuota,
+        currentAccepted,
+        paymentMethod,
+        isApproved,
+        paidAt,
+        gatewayData,
         amount,
-        employer_email,
+        employerEmail,
+        fieldsOfWork,
     });
 }
 
 exports.Delete = async (req, res) => {
-    const {task_id} = req.body;
+    const {taskId} = req.body;
     // TODO : escape string
-    const rows = await MySQL.Query(`delete from Task where task_id = ${task_id}`);
+    const rows = await MySQL.Query(`delete from Task where task_id = ${taskId}`);
     if (rows.affectedRows == 1) {
         res.status(200).send();
     } else {
