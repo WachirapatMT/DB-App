d5b0871bdbcdbf010052800b4e1b93ecba23bf24
Rock <nattanon.hu@gmail.com>
2020-11-11 16:09:58 +0700

    ✨ implement EmployerJob page

--- src/pages/EmployerJob.js
+++ src/pages/EmployerJob.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { PageHeader, Card, Button } from 'antd';
+import { JobAdsForm } from '../components/JobAdsForm';
 
 const EmployerJob = () => {
-  return <div>Employer Job</div>;
+  const [loading, setLoading] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const history = useHistory();
+  const { email } = useParams();
+
+  const onEdit = (values) => {
+    setLoading(true);
+    // NOTE demo
+    setTimeout(() => {
+      console.log(values);
+      setLoading(false);
+      setIsEdit(false);
+    }, 2000);
+    // TODO integrate with backend
+  };
+
+  const JobDetails = () => <Card>job detail</Card>;
+
+  return (
+    <>
+      <PageHeader
+        title="Job Advertisement details"
+        onBack={() => {
+          history.push(`/employer/${email}`);
+        }}
+        extra={
+          isEdit
+            ? []
+            : [
+                <Button type="primary" onClick={() => setIsEdit(true)}>
+                  Edit
+                </Button>,
+              ]
+        }
+      />
+      {isEdit ? (
+        <Card>
+          <JobAdsForm
+            onSubmit={onEdit}
+            loading={loading}
+            customButtons={[
+              <Button onClick={() => setIsEdit(false)}>Cancel</Button>,
+            ]}
+          />
+        </Card>
+      ) : (
+        <JobDetails />
+      )}
+    </>
+  );
 };
 
 export default EmployerJob;
