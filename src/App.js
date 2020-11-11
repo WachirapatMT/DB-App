ecd3a30dff33eaee095bc2d3379c47ab06f56d42
techid2000 <techid2000@gmail.com>
2020-11-11 10:55:56 +0700

    ✨ add AppLayout to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
 import { Employer, Landing, Student } from './routes';
 
 const App = () => {
-  // NOTE: Insert layout components here
   return (
     <div>
-      App
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route path="/student/:email">
-          <Student />
-        </Route>
-        <Route path="/employer/:email">
-          <Employer />
-        </Route>
-      </Switch>
+      <AppLayout>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/student/:email">
+            <Student />
+          </Route>
+          <Route path="/employer/:email">
+            <Employer />
+          </Route>
+        </Switch>
+      </AppLayout>
     </div>
   );
 };
