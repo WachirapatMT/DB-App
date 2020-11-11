88d4663d288b81150167b430677d5f63e48fa34b
techid2000 <techid2000@gmail.com>
2020-11-11 20:31:45 +0700

    ♻️ prepare EmployerJobList load async

--- src/components/EmployerJobList.js
+++ src/components/EmployerJobList.js
@@ -1,59 +1,56 @@
+import React, { useState, useEffect } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, List, Row, Typography } from 'antd';
-import React from 'react';
+import { Button, List, Row, Spin, Typography } from 'antd';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import { EmployerJobItem } from '.';
+import { getJobList } from '../api/employer';
 
-const jobList = [
-  {
-    id: 'job-1',
-    title: 'Create a New Prototype of Macbook Pro 2025',
-    description:
-      'The prototype should be the same as 2024, just some few tweaks.',
-    fieldOfWork: ['Design', 'Hardware'],
-    minQuota: 5,
-    maxQuota: 7,
-    minCompensation: 50000,
-    maxCompensation: 100000,
-  },
-  {
-    id: 'job-2',
-    title: 'Debug NASA Spaceship',
-    description: 'NASA kor pa ter glub ma mai dai T T',
-    fieldOfWork: ['Repairment', 'Hardware'],
-    minQuota: 10,
-    maxQuota: 20,
-    minCompensation: 100000,
-    maxCompensation: 300000,
-  },
-];
+const loadJobList = async (setJobList, setLoading) => {
+  setJobList(await getJobList());
+  setLoading(false);
+};
 
 const EmployerJobList = () => {
   const { url } = useRouteMatch();
   const history = useHistory();
 
-  return (
-    <List
-      itemLayout="vertical"
-      dataSource={jobList}
-      header={
-        <Typography.Title level={3}>Your job advertisements</Typography.Title>
-      }
-      footer={
-        <Row justify="center">
-          <Button
-            shape="round"
-            icon={<PlusOutlined />}
-            size="large"
-            onClick={() => history.push(`${url}/job/create`)}
-          >
-            Add a new job
-          </Button>
-        </Row>
-      }
-      renderItem={(job) => <EmployerJobItem job={job} />}
-    />
-  );
+  const [loading, setLoading] = useState(true);
+  const [jobList, setJobList] = useState([]);
+
+  useEffect(() => {
+    loadJobList(setJobList, setLoading);
+  }, []);
+
+  if (loading) {
+    return (
+      <Row justify="center">
+        <Spin />
+      </Row>
+    );
+  } else {
+    return (
+      <List
+        itemLayout="vertical"
+        dataSource={jobList}
+        header={
+          <Typography.Title level={4}>Your job advertisements</Typography.Title>
+        }
+        footer={
+          <Row justify="center">
+            <Button
+              shape="round"
+              icon={<PlusOutlined />}
+              size="large"
+              onClick={() => history.push(`${url}/job/create`)}
+            >
+              Add a new job
+            </Button>
+          </Row>
+        }
+        renderItem={(job) => <EmployerJobItem job={job} />}
+      />
+    );
+  }
 };
 
 export default EmployerJobList;
