453d15822f145be4d2f3bd211dec01f0129164bf
Wachirapat M <wachi_tony@yahoo.co.th>
2020-11-18 12:12:07 +0700

    connect employer getJobById to api

--- src/api/employer.js
+++ src/api/employer.js
@@ -6,7 +6,25 @@ axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export const getJobList = async (email) => {
   try {
-    const response = await axios.get(`/task`);
+    const response = await axios.get(`/task`, {params: {employerEmail: email}});
+    return response.data;
+  } catch(err) {
+    return err;
+  }
+};
+
+export const getJobById = async (jobId) => {
+  try {
+    const response = await axios.get(`/task`, {params: {taskId: jobId}});
+    return response.data;
+  } catch(err) {
+    return err;
+  }
+};
+    return response.data;
+    return err;
+  }
+};
     return response.data;
   } catch(err) {
     return err;
